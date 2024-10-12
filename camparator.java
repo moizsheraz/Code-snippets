0c5a4423a33395622220d1dc7076ebb017691979
Moiz Sheraz <user146712@gmail.com>
2024-10-12 16:11:41 +0500

    Update camparator.java

--- camparator.java
+++ camparator.java
@@ -1,2 +1,6 @@
 Arrays.sort(pairs, (a, b) -> a[1] - b[1]);
 // sort array on the basis of second element of each pair (in ascending order)
+
+// sort  the 2D Array on the basis of first index
+
+Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0])); 
